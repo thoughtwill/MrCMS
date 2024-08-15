e1a5c5cd067c62e51d75ad14133361b5d727e4ad
Dalir <dalir.bajelani@thought.co.uk>
2024-08-15 09:56:11 +0100

    add back removed sitemap controller

---
+++ src/Lib/MrCMS/Website/Controllers/SitemapController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MrCMS.Services;
+using MrCMS.Services.Sitemaps;
+
+namespace MrCMS.Website.Controllers;
+
+public class SitemapController : MrCMSUIController
+{
+    private const string SitemapUrl = "sitemap.xml";
+    private readonly IGetSitemapPath _getSitemapPath;
+    private readonly ICurrentSiteLocator _siteLocator;
+
+    public SitemapController(IGetSitemapPath getSitemapPath, ICurrentSiteLocator siteLocator)
+    {
+        _getSitemapPath = getSitemapPath;
+        _siteLocator = siteLocator;
+    }
+
+    [Route(SitemapUrl)]
+    public ActionResult Show()
+    {
+        var site = _siteLocator.GetCurrentSite();
+        if (!_getSitemapPath.FileExists(site))
+            return new EmptyResult();
+
+        return PhysicalFile(_getSitemapPath.GetAbsolutePath(site), "application/xml");
+    }
+
+}
