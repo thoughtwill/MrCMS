9c7c817880a71227f9bb24ac8aff08af04fd0702
Dalir <dalir.bajelani@thought.co.uk>
2024-12-17 09:15:11 +0000

    Improved array index condition handling in templates
    The ArrayIndexConditionTemplateTokenProvider has been updated to handle more complex conditions. Previously, it only supported simple integer comparisons. Now, it uses the mXparser library to evaluate mathematical expressions, allowing for more flexible and dynamic conditions.
    
    This change involved modifying the HtmlPattern property to accept a condition string instead of an integer value. The ViewRenderAsync method was also updated to parse and evaluate this condition using mXparser.
    
    A new package reference for mXparser was added to MrCMS.csproj as part of this update. This enhancement provides greater flexibility when defining template token conditions based on array indices.

--- src/Lib/MrCMS/ContentTemplates/ContentTemplateTokenProviders/ArrayIndexConditionTemplateTokenProvider.cs
+++ src/Lib/MrCMS/ContentTemplates/ContentTemplateTokenProviders/ArrayIndexConditionTemplateTokenProvider.cs
@@ -3,18 +3,19 @@ using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using MrCMS.ContentTemplates.ContentTemplateTokenProviders.Base;
 using MrCMS.ContentTemplates.Models;
+using org.mariuszgromada.math.mxparser;
 
 namespace MrCMS.ContentTemplates.ContentTemplateTokenProviders;
 
 public class ArrayIndexConditionTemplateTokenProvider : ContentTemplateTokenProvider
 {
     public override string Icon => "fa fa-indent";
-    public override string HtmlPattern => $"[{Name} name=\"{Name}1\" if=\"0\" then=\"active\"]";
+    public override string HtmlPattern => $"[{Name} name=\"{Name}1\" if=\"index == 0\" then=\"active\"]";
 
     public override async Task<IHtmlContent> ViewRenderAsync(IHtmlHelper helper, ViewRenderElementProperty property)
     {
         var value = "";
-        int? equalValue = null;
+        var result = false;
         if (property.Attributes != null)
         {
             foreach (var attr in property.Attributes)
@@ -22,15 +23,13 @@ public class ArrayIndexConditionTemplateTokenProvider : ContentTemplateTokenProv
                 switch (attr.Key)
                 {
                     case "if":
-                        if (int.TryParse(attr.Value, out int parsedValue))
-                        {
-                            equalValue = parsedValue;
-                        }
-                        else
-                        {
-                            equalValue = null;
-                        }
-
+                        string condition = attr.Value;
+                        Expression expression = new Expression(condition);
+                        
+                        Argument index = new Argument($"index = {property.Index ?? 0}");
+                        expression.addArguments(index);
+                        
+                        result = expression.calculate() != 0;
                         break;
                     case "then":
                         value = attr.Value;
@@ -39,7 +38,7 @@ public class ArrayIndexConditionTemplateTokenProvider : ContentTemplateTokenProv
             }
         }
 
-        if (equalValue.HasValue && property.Index.HasValue && equalValue == property.Index)
+        if (result)
         {
             return await Task.FromResult(new HtmlString(value));
         }
