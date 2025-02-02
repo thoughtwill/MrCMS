89f1bdc3e7a474f9b25a76188b9d0d9f038aa2fa
Dalir Bajelani <dalir.bajelani@thought.co.uk>
2025-02-02 13:23:30 +0000

    remove api url from open ai settings

--- src/Lib/MrCMS/AI/Settings/OpenAiSettings.cs
+++ src/Lib/MrCMS/AI/Settings/OpenAiSettings.cs
@@ -5,12 +5,10 @@ public class OpenAiSettings : AiSettingsBase
     public OpenAiSettings()
     {
         Model = "gpt-4o";
-        ApiUrl = "https://api.openai.com/v1/chat/completions";
         ApiKey = string.Empty;
     }
 
     public string Model { get; set; }
-    public string ApiUrl { get; set; }
     public string ApiKey { get; set; }
 
     public override bool RenderInSettings => true;
